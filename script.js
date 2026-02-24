b0c4683a898235d63d2c23edca641d9c675e3a77
Adekoya <oladayo@spotter.ai>
2026-02-24 13:58:41 +0100

    init basic functions

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 const body = document.querySelector('body');
+let firstNum;
+let secondNum;
+let operator;
 
-console.log('hello shendow')
+
+console.log('operate', operate('/', 100, 2))
+
+
+
+// Util functions
+function operate(operator, num1, num2) {
+    let sum;
+    switch (operator) {
+        case '+':
+            sum = add(num1, num2)
+            break;
+        case '-':
+            sum = subtract(num1,num2)
+            break;
+         case '*':
+            sum = multiply(num1, num2)
+            break;
+        case '/':
+            sum = divide(num1,num2)
+            break;
+    
+        default:
+            break;
+    }
+    return sum
+
+}
+function add(a, b) {
+    return a + b;
+}
+function subtract(a, b) {
+    return a - b;
+}
+function multiply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
